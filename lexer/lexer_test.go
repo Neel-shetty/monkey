a27918d9db7cca3c6118df1332675a7b9cb0abfd
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-04-26 22:42:52 +0530

    doesnt-work

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,61 +6,6 @@ import (
 	"github.com/Neel-shetty/monkey/token"
 )
 
-type Lexer struct {
-	input        string
-	position     int
-	nextPosition int
-	ch           byte
-}
-
-func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
-	return l
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
-func (l *Lexer) readChar() {
-	if l.nextPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.nextPosition]
-	}
-	l.position = l.nextPosition
-	l.nextPosition += 1
-}
-
-func (l *Lexer) NextToken() token.Token {
-	var tok token.Token
-
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
-		tok.Literal = ""
-		tok.Type = token.EOF
-	}
-	l.readChar()
-	return tok
-}
-
 type test struct {
 	expectedType    token.TokenType
 	expectedLiteral string
@@ -68,22 +13,61 @@ type test struct {
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
-	let ten = 10;
+let ten = 10;
 
-	let add = fn(x, y) {
-		x + y
-	};
+let add = fn(x, y) {
+  x + y;
+};
 
-	let result = add(five,ten);
-	`
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+`
 	tests := []test{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
 		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
