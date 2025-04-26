7cab46107182ce225fe228ec3cda702234ccdf5e
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-04-26 21:00:41 +0530

    lexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Neel-shetty/monkey
+
+go 1.24.1
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,104 @@
 package lexer
 
 import (
-	"monkey/token"
 	"testing"
+
+	"github.com/Neel-shetty/monkey/token"
 )
+
+type Lexer struct {
+	input        string
+	position     int
+	nextPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readChar() {
+	if l.nextPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.nextPosition]
+	}
+	l.position = l.nextPosition
+	l.nextPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+type test struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+
+	let add = fn(x, y) {
+		x + y
+	};
+
+	let result = add(five,ten);
+	`
+	tests := []test{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
