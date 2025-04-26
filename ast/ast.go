d823ff342bffe6e5511f470fb47fad36914a94fd
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-04-26 23:47:58 +0530

    Updates

--- ast/ast.go
+++ ast/ast.go
@@ -20,10 +20,24 @@ type Program struct {
 	Statements []Statement
 }
 
+type Identifier struct {
+	Token token.Token // the identifier token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
+
 type LetStatement struct {
 	Token token.Token // the 'let' token
 	Name  *Identifier
 	Value Expression
+}
+
+func (ls *LetStatement) StatementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
 }
 
 func (p *Program) TokenLiteral() string {
